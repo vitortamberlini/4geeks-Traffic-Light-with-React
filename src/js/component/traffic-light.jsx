ec2af0fa2b0fb88def630c5b86bcbfe4bf6b8a79
vitortamberlini <vitor.tamberlini@outlook.com>
2022-01-28 01:12:26 +0000

    WIP - Created traffic light structure

--- src/js/component/traffic-light.jsx
+++ src/js/component/traffic-light.jsx
@@ -1,7 +1,18 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 const TrafficLight = () => {
 	return (
 		<>
-			<p>Hello World!</p>
+			<Container className="d-flex flex-column align-items-center">
+				<Row id="handle">
+					<Col className="traffic-color"></Col>
+				</Row>
+				<Row id="traffic-box">
+					<Col className="traffic-color rounded"></Col>
+				</Row>
+			</Container>
 		</>
 	);
 };
